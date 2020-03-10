b6e5663a523f6e925a1a85fdc1831c2f3b677c98
Jonathan Williams <ewilliams@triumf.ca>
2020-03-10 12:53:01 -0700

    Fixed some errors from previous commit.

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 //get a formatted string with a value and its uncertainty, 
 //properly rounded using the '20 rule' for reporting uncertainties
 void getFormattedValAndUncertainty(const double val, const double err, char *str, const int strLength){
-  
+
   if(err < 0){
     //invalid error
     snprintf(str,strLength,"Negative err!");
@@ -14,13 +14,13 @@ void getFormattedValAndUncertainty(const double val, const double err, char *str
 
   //get the position of the first sig fig in the error
   int sigf = 0;
-  if(errc > 0){
-    while(errc >= 19.5){
+  if(fabs(errc) > 1){
+    while(fabs(errc) >= 19.5){
       errc /= 10;
       sigf++;
     }
-  }else if(errc < 0){
-    while(errc <= 1.95){
+  }else if((fabs(errc) < 1)&&(errc != 0)){
+    while(fabs(errc) <= 1.95){
       errc *= 10;
       sigf--;
     }
@@ -35,14 +35,19 @@ void getFormattedValAndUncertainty(const double val, const double err, char *str
   //get the first digits of the error
   errc = floor(errc);
 
+  //printf("sigf: %i, errc = %f\n",sigf,errc);
+
   if(val == 0){
     snprintf(str,strLength,"0(0)");
   }else if(fabs(val) > 0.001){
     //use normal notation
-    snprintf(str,strLength,"%0.0f(%0.0f)",valc*pow(10,sigf),errc*pow(10,sigf));
+    if(sigf < 0)
+      snprintf(str,strLength,"%0.*f(%.0f)",-1*sigf,valc*pow(10,sigf),errc);
+    else
+      snprintf(str,strLength,"%0.0f(%.0f)",valc*pow(10,sigf),errc*pow(10,sigf));
   }else{
     //use scientific notation
-    snprintf(str,strLength,"%0.0f(%0.0f)E%i",valc,errc,sigf);
+    snprintf(str,strLength,"%0.0f(%.0f)E%i",valc,errc,sigf);
   }
 
 }
